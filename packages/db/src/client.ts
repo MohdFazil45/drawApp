139113143a0f928d73dd1eedd9da3752e25fb413
MohdFazil45 <mdfazil846@gmail.com>
2026-03-03 11:59:33 +0530

    fixed #11

---
+++ packages/db/src/client.ts
@@ -0,0 +1,13 @@
+export { Prisma } from "./generated/prisma/client"; // exports instance of prisma
+export * from "./generated/prisma/client.js"; // exports generated types from prisma
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,11 +1,2 @@
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '../../.env') });
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "./generated/prisma/client";
-
-const connectionString = `${process.env.DATABASE_URL}`;
-const adapter = new PrismaPg({ connectionString });
-export const prisma = new PrismaClient({ adapter });
-
-
+export { prisma } from "./client.js"; // exports instance of prisma
+export * from "./generated/prisma/client.js"; // exports generated types from prisma
