bfa784d80b3e5df08353d5a9217aa71c385a88b2
MohdFazil45 <mdfazil846@gmail.com>
2026-03-03 15:37:09 +0530

    fixed #20

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,10 +1,10 @@
-export { Prisma } from "../dist/generated/prisma/client.js"; // exports instance of prisma
-export * from "../dist/generated/prisma/client.js"; // exports generated types from prisma
+export { Prisma } from "./generated/prisma/client.js"; // exports instance of prisma
+export * from "./generated/prisma/client.js"; // exports generated types from prisma
 import dotenv from "dotenv";
 import path from "path";
 dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../dist/generated/prisma/client.js";
+import { PrismaClient } from "./generated/prisma/client.js";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 const adapter = new PrismaPg({ connectionString });
